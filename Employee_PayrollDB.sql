2f17045a9ac5ebb0a696197470073f79accf5972
manjugoutham <gouthamy219@gmail.com>
2022-01-15 20:09:19 +0530

    Ability to create a employee payroll table in the payroll service database.

--- Employee_PayrollDB.sql
+++ Employee_PayrollDB.sql
@@ -1,23 +1,25 @@
-.............UC1..............
+........UC2.........
 
-mysql> show databases;
-+--------------------+
-| Database           |
-+--------------------+
-| information_schema |
-| mysampledb         |
-| mysql              |
-| performance_schema |
-| sakila             |
-| sys                |
-| world              |
-+--------------------+
-7 rows in set (0.10 sec)
+mysql> create table employee_payroll;
+ERROR 4028 (HY000): A table must have at least one visible column.
+mysql> create table employee_payroll
+    -> (id int(10) primary key,
+    -> name varchar(20) not null);
+Query OK, 0 rows affected, 1 warning (2.36 sec)
 
-mysql> create database payroll_Service;
-Query OK, 1 row affected (0.17 sec)
-
-mysql> use payroll_Service;
-Database changed
 mysql> show tables;
-Empty set (0.24 sec)
++---------------------------+
+| Tables_in_payroll_service |
++---------------------------+
+| employee_payroll          |
++---------------------------+
+1 row in set (0.09 sec)
+
+mysql> desc employee_payroll;
++-------+-------------+------+-----+---------+-------+
+| Field | Type        | Null | Key | Default | Extra |
++-------+-------------+------+-----+---------+-------+
+| id    | int         | NO   | PRI | NULL    |       |
+| name  | varchar(20) | NO   |     | NULL    |       |
++-------+-------------+------+-----+---------+-------+
+2 rows in set (0.56 sec)
