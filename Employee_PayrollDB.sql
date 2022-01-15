3e8defea86ec29edfabeadf68d712f3b327b72c6
manjugoutham <gouthamy219@gmail.com>
2022-01-15 20:16:16 +0530

    Ability to retrieve salary data for a particular employee as well as all employees who have joined in a particular data range from the payroll service database

--- Employee_PayrollDB.sql
+++ Employee_PayrollDB.sql
@@ -1,4 +1,18 @@
-mysql> select * from employee_payroll;
+mysql> select salary from employee_payroll where name = 'smith';
++--------+
+| salary |
++--------+
+|  22300 |
++--------+
+1 row in set (0.07 sec)
+
+mysql> SELECT * FROM employee_payroll WHERE start_date
+    -> BETWEEN 'STARTING_DATE_TIME' AND 'ENDING_DATE_TIME';
+Empty set, 8 warnings (0.05 sec)
+
+mysql> SELECT * FROM employee_payroll WHERE start between CAST('2022-01-10' as DATE) and DATE(NOW());
+ERROR 1054 (42S22): Unknown column 'start' in 'where clause'
+mysql> SELECT * FROM employee_payroll WHERE start_date between CAST('2022-01-10' as DATE) and DATE(NOW());
 +----+------------+----------+------------+--------+
 | id | name       | address  | start_date | salary |
 +----+------------+----------+------------+--------+
@@ -7,3 +21,4 @@ mysql> select * from employee_payroll;
 |  3 | Manju      | Bnaglore | 2022-01-10 |  22000 |
 |  4 | smith      | kolar    | 2022-01-11 |  22300 |
 +----+------------+----------+------------+--------+
+4 rows in set (0.04 sec)
