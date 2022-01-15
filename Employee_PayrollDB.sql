d8c2ee10127b9dbb722928b805ce707fea7458c3
manjugoutham <gouthamy219@gmail.com>
2022-01-15 20:26:14 +0530

    Ability to extend employee_payroll table to have Basic Pay, Deductions, Taxable Pay, Income Tax, Net Pay

--- Employee_PayrollDB.sql
+++ Employee_PayrollDB.sql
@@ -1,26 +1,21 @@
-................//UC8............
+................UC9........
+
+INSERT INTO employee_payrolls(EmployeeID,EmployeeName,PhoneNumber,D' at line 1
+mysql> INSERT INTO employee_payrolls(EmployeeID,EmployeeName,PhoneNumber,Department,gender,BasicPay,Deductions,TaxablePay,Tax,NetPay,address,start_date,City,Country,salary)VALUES(1,'Gouthum','8105456712','Devloper Team','M',25000,5000,2000,3000,5000,'Near_RT nagar banglore','2021-10-25','Bnaglore','India',30000);
+Query OK, 1 row affected (0.09 sec)
+
+mysql> INSERT INTO employee_payrolls(EmployeeID,EmployeeName,PhoneNumber,Department,gender,BasicPay,Deductions,TaxablePay,Tax,NetPay,address,start_date,City,Country,salary)VALUES(2,'Terissa','8105456712','sales department','M',25000,5000,2000,3000,5000,'Near_RT nagar banglore','2021-10-25','banglore','India',30000);
+Query OK, 1 row affected (0.56 sec)
+
+mysql> INSERT INTO employee_payrolls(EmployeeID,EmployeeName,PhoneNumber,Department,gender,BasicPay,Deductions,TaxablePay,Tax,NetPay,address,start_date,City,Country,salary)VALUES(3,'Terissa','8105456712','marketing','M',25000,5000,2000,3000,5000,'Near_RT nagar banglore','2021-10-25','banglore','India',30000);
+Query OK, 1 row affected (0.06 sec)
 
 
-
-
-
-mysql> desc employee_payrolls;
-+--------------+--------------+------+-----+---------+-------+
-| Field        | Type         | Null | Key | Default | Extra |
-+--------------+--------------+------+-----+---------+-------+
-| EmployeeID   | int          | NO   | PRI | NULL    |       |
-| EmployeeName | varchar(200) | YES  |     | NULL    |       |
-| Phonenumber  | varchar(200) | YES  |     | NULL    |       |
-| Department   | varchar(200) | YES  |     | NULL    |       |
-| gender       | char(1)      | YES  |     | NULL    |       |
-| BasicPay     | float        | YES  |     | NULL    |       |
-| Deductions   | float        | YES  |     | NULL    |       |
-| TaxablePay   | float        | YES  |     | NULL    |       |
-| Tax          | float        | YES  |     | NULL    |       |
-| NetPay       | float        | YES  |     | NULL    |       |
-| address      | varchar(200) | YES  |     | NULL    |       |
-| start_date   | date         | YES  |     | NULL    |       |
-| City         | varchar(200) | YES  |     | NULL    |       |
-| Country      | varchar(200) | YES  |     | NULL    |       |
-| salary       | int          | YES  |     | NULL    |       |
-+--------------+--------------+------+-----+---------+-------+
+mysql> select * from employee_payrolls;
++------------+--------------+-------------+------------------+--------+----------+------------+------------+------+--------+------------------------+------------+----------+---------+--------+
+| EmployeeID | EmployeeName | Phonenumber | Department       | gender | BasicPay | Deductions | TaxablePay | Tax  | NetPay | address                | start_date | City     | Country | salary |
++------------+--------------+-------------+------------------+--------+----------+------------+------------+------+--------+------------------------+------------+----------+---------+--------+
+|          1 | Gouthum      | 8105456712  | Devloper Team    | M      |    25000 |       5000 |       2000 | 3000 |   5000 | Near_RT nagar banglore | 2021-10-25 | Bnaglore | India   |  30000 |
+|          2 | Terissa      | 8105456712  | sales department | M      |    25000 |       5000 |       2000 | 3000 |   5000 | Near_RT nagar banglore | 2021-10-25 | banglore | India   |  30000 |
+|          3 | Terissa      | 8105456712  | marketing        | M      |    25000 |       5000 |       2000 | 3000 |   5000 | Near_RT nagar banglore | 2021-10-25 | banglore | India   |  30000 |
++------------+--------------+-------------+------------------+--------+----------+------------+------------+------+--------+------------------------+------------+----------+---------+--------+
