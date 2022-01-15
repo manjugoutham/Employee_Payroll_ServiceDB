d3df639e71f08b106459cca89b2811919500b1a9
manjugoutham <gouthamy219@gmail.com>
2022-01-15 20:19:40 +0530

    Ability to add Gender to Employee Payroll Table

--- Employee_PayrollDB.sql
+++ Employee_PayrollDB.sql
@@ -1,24 +1,41 @@
-mysql> select salary from employee_payroll where name = 'smith';
-+--------+
-| salary |
-+--------+
-|  22300 |
-+--------+
-1 row in set (0.07 sec)
+//.....................UC6...........................
 
-mysql> SELECT * FROM employee_payroll WHERE start_date
-    -> BETWEEN 'STARTING_DATE_TIME' AND 'ENDING_DATE_TIME';
-Empty set, 8 warnings (0.05 sec)
 
-mysql> SELECT * FROM employee_payroll WHERE start between CAST('2022-01-10' as DATE) and DATE(NOW());
-ERROR 1054 (42S22): Unknown column 'start' in 'where clause'
-mysql> SELECT * FROM employee_payroll WHERE start_date between CAST('2022-01-10' as DATE) and DATE(NOW());
-+----+------------+----------+------------+--------+
-| id | name       | address  | start_date | salary |
-+----+------------+----------+------------+--------+
-|  1 | Gowthamraj | Bnaglore | 2022-01-12 |  25000 |
-|  2 | Gowtham    | Bnaglore | 2022-01-12 |  20000 |
-|  3 | Manju      | Bnaglore | 2022-01-10 |  22000 |
-|  4 | smith      | kolar    | 2022-01-11 |  22300 |
-+----+------------+----------+------------+--------+
-4 rows in set (0.04 sec)
+mysql> alter table employee_payroll add gender char(1)not null after name;
+Query OK, 0 rows affected (4.55 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> desc employee_payroll;
++------------+--------------+------+-----+---------+-------+
+| Field      | Type         | Null | Key | Default | Extra |
++------------+--------------+------+-----+---------+-------+
+| id         | int          | NO   | PRI | NULL    |       |
+| name       | varchar(20)  | NO   |     | NULL    |       |
+| gender     | char(1)      | NO   |     | NULL    |       |
+| address    | varchar(100) | YES  |     | NULL    |       |
+| start_date | date         | YES  |     | NULL    |       |
+| salary     | int          | YES  |     | NULL    |       |
++------------+--------------+------+-----+---------+-------+
+6 rows in set (0.06 sec)
+
+mysql> alter table employee_payroll add gender char(1) after name;
+ERROR 1060 (42S21): Duplicate column name 'gender'
+mysql> update employee_payroll set gender='M' where name ='Gowtham' or name = 'manju;'
+    -> ;
+Query OK, 1 row affected (0.22 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> update employee_payroll set gender='M' where name ='Gowtham' or name = 'manju';
+Query OK, 1 row affected (0.15 sec)
+Rows matched: 2  Changed: 1  Warnings: 0
+
+mysql> SELECT * FROM employee_payroll;
++----+------------+--------+----------+------------+--------+
+| id | name       | gender | address  | start_date | salary |
++----+------------+--------+----------+------------+--------+
+|  1 | Gowthamraj |        | Bnaglore | 2022-01-12 |  25000 |
+|  2 | Gowtham    | M      | Bnaglore | 2022-01-12 |  20000 |
+|  3 | Manju      | M      | Bnaglore | 2022-01-10 |  22000 |
+|  4 | smith      |        | kolar    | 2022-01-11 |  22300 |
++----+------------+--------+----------+------------+--------+
+4 rows in set (0.00 sec)
