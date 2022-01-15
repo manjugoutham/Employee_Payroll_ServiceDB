4e4f9d1c4ca3952219a62357c084e5ec508848e3
manjugoutham <gouthamy219@gmail.com>
2022-01-15 20:21:40 +0530

    Ability to find sum, average, min, max and number of male and female employees

--- Employee_PayrollDB.sql
+++ Employee_PayrollDB.sql
@@ -1,41 +1,93 @@
-//.....................UC6...........................
+//.............UC7.......................
 
 
-mysql> alter table employee_payroll add gender char(1)not null after name;
-Query OK, 0 rows affected (4.55 sec)
-Records: 0  Duplicates: 0  Warnings: 0
+mysql> select sum(salary) from employee_payroll where gender='M' group by gender;
++-------------+
+| sum(salary) |
++-------------+
+|       42000 |
++-------------+
+1 row in set (0.03 sec)
 
-mysql> desc employee_payroll;
-+------------+--------------+------+-----+---------+-------+
-| Field      | Type         | Null | Key | Default | Extra |
-+------------+--------------+------+-----+---------+-------+
-| id         | int          | NO   | PRI | NULL    |       |
-| name       | varchar(20)  | NO   |     | NULL    |       |
-| gender     | char(1)      | NO   |     | NULL    |       |
-| address    | varchar(100) | YES  |     | NULL    |       |
-| start_date | date         | YES  |     | NULL    |       |
-| salary     | int          | YES  |     | NULL    |       |
-+------------+--------------+------+-----+---------+-------+
-6 rows in set (0.06 sec)
+mysql> select count(name) from employee_payroll;
++-------------+
+| count(name) |
++-------------+
+|           4 |
++-------------+
+1 row in set (0.20 sec)
 
-mysql> alter table employee_payroll add gender char(1) after name;
-ERROR 1060 (42S21): Duplicate column name 'gender'
-mysql> update employee_payroll set gender='M' where name ='Gowtham' or name = 'manju;'
-    -> ;
-Query OK, 1 row affected (0.22 sec)
-Rows matched: 1  Changed: 1  Warnings: 0
+mysql> select avg(salary) from employee_payroll;
++-------------+
+| avg(salary) |
++-------------+
+|  22325.0000 |
++-------------+
+1 row in set (0.00 sec)
 
-mysql> update employee_payroll set gender='M' where name ='Gowtham' or name = 'manju';
-Query OK, 1 row affected (0.15 sec)
-Rows matched: 2  Changed: 1  Warnings: 0
+mysql> select min(salary) from employee_payroll;
++-------------+
+| min(salary) |
++-------------+
+|       20000 |
++-------------+
+1 row in set (0.04 sec)
 
-mysql> SELECT * FROM employee_payroll;
-+----+------------+--------+----------+------------+--------+
-| id | name       | gender | address  | start_date | salary |
-+----+------------+--------+----------+------------+--------+
-|  1 | Gowthamraj |        | Bnaglore | 2022-01-12 |  25000 |
-|  2 | Gowtham    | M      | Bnaglore | 2022-01-12 |  20000 |
-|  3 | Manju      | M      | Bnaglore | 2022-01-10 |  22000 |
-|  4 | smith      |        | kolar    | 2022-01-11 |  22300 |
-+----+------------+--------+----------+------------+--------+
-4 rows in set (0.00 sec)
+mysql> select max(salary) from employee_payroll;
++-------------+
+| max(salary) |
++-------------+
+|       25000 |
++-------------+
+1 row in set (0.00 sec)
+
+mysql> select count(name) from employee_payroll where gender = 'F';
++-------------+
+| count(name) |
++-------------+
+|           1 |
++-------------+
+1 row in set (0.00 sec)
+
+mysql> select count(name) from employee_payroll where gender = 'F' group by gender;
++-------------+
+| count(name) |
++-------------+
+|           1 |
++-------------+
+1 row in set (0.00 sec)
+
+mysql> select count(id) from employee_payroll where gender = 'M' group by salary;
++-----------+
+| count(id) |
++-----------+
+|         1 |
+|         1 |
++-----------+
+2 rows in set (0.02 sec)
+
+mysql> select count(name) from employee_payroll where gender = 'M' group by salary;
++-------------+
+| count(name) |
++-------------+
+|           1 |
+|           1 |
++-------------+
+2 rows in set (0.00 sec)
+
+mysql> select avg(salary) from employee_payroll where gender = 'F' group by id;
++-------------+
+| avg(salary) |
++-------------+
+|  22300.0000 |
++-------------+
+1 row in set (0.00 sec)
+
+mysql> select avg(salary) from employee_payroll where gender = 'M' group by id;
++-------------+
+| avg(salary) |
++-------------+
+|  20000.0000 |
+|  22000.0000 |
++-------------+
+2 rows in set (0.00 sec)
